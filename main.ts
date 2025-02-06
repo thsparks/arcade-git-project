e8f73501f1465e1b9ad8c75bb1ad015df264a0d9
Thomas Sparks <69657545+thsparks@users.noreply.github.com>
2025-02-06 10:49:29 -0800

    Add some blocks

--- main.ts
+++ main.ts
@@ -1,1 +1,21 @@
-
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    music.play(music.stringPlayable("C D E F E F G A ", 120), music.PlaybackMode.UntilDone)
+})
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . 3 . 3 . . . . . . 
+    . . . . . . . 3 . 3 . . . . . . 
+    . . . . . . . 3 . 3 . . . . . . 
+    . . . . . . . 3 . 3 . . 3 . . . 
+    . . . . . . . . . . . . 3 . . . 
+    . . . 3 . . . . . . . . 3 . . . 
+    . . . 3 . . . . . . . . 3 . . . 
+    . . . 3 . . . . . . . 3 . . . . 
+    . . . 3 . . . . . . . 3 . . . . 
+    . . . 3 . . . . . . . 3 . . . . 
+    . . . 3 3 . . . . . . 3 . . . . 
+    . . . . . 3 3 3 3 3 3 3 . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
